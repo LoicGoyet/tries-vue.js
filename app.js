d7821c2a1b88faf29697cc176dc68310553ac86b
Loïc Goyet <loic@appventus.com>
2017-01-06 16:53:44 +0100

    simple todo list

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
-    title: 'You loaded this page on ' + new Date()
+    heading: {
+      text: 'Loïc tries vue.js',
+      title: 'You loaded this page on ' + new Date(),
+    },
+    todoNewEntry: '',
+    todos: [
+      {
+        text: 'Learn JavaScript',
+        checked: false,
+      },
+      {
+        text: 'Learn Vue',
+        checked: false,
+      },
+      {
+        text: 'Build something awesome',
+        checked: false,
+      }
+    ]
+  },
+  methods: {
+    reverseHeading: function() {
+      this.heading.text = this.heading.text.split(' ').reverse().join(' ');
+    },
+    addTodoEntry: function() {
+      if (!this.todoNewEntry.length) return false;
+
+      this.todos.push({
+        text: this.todoNewEntry,
+        checked: false,
+      });
+      this.todoNewEntry = '';
+    },
+    toggleTodoEntryCheck: function(todo) {
+      return todo.checked = !todo.checked;
+    }
   }
 })
